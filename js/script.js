80f73ddcc49b522f1498e3d1589b2196c0df1541
Crisleop21 <leonelporoj@gmail.com>
2021-12-09 14:40:43 -0600

    agregue la funcion jquery

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$( '.toggle').click(function(){
+    $('.form-register').animate({
+        height: "toggle",
+        'padding-top': 'toggle',
+        'padding-bottom': 'toggle',
+        opacity: 'toggle'
+
+    })
+});
