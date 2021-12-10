54f0a6524012708d108de5d3cd920502542a1512
Crisleop21 <leonelporoj@gmail.com>
2021-12-09 19:52:42 -0600

    agregue bootstrap para los botones

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ $( '.toggle').click(function(){
 
     })
 });
+
+const usuario = document.getElementById("Ingrese Usuario");
+const password = document.getElementById("Ingrese Contraseña");
+
+function iniciarSesion(){
+    console.log("Usuario: " + usuario.value);
+    console.log("Contraseña: " + password.value);
+    alert("Se ha iniciado la sesión");
+}
